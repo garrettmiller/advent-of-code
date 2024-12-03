38205194b47331f25c26ea3db9deab98b94c936d
Garrett Miller <garrettmiller@gmail.com>
2024-12-03 00:02:35 -0800

    Solved p03-2, not the prettiest

--- 2024/p03-2.py
+++ 2024/p03-2.py
@@ -20,16 +20,19 @@ Handle the new instructions; what do you get if you add up all of the results of
 import re
 
 totalProduct = 0
-enabled = True
 do_positions = []
 dont_positions = []
+enabled_ranges = []
 
 # Define a regex pattern for valid instructions: mul(X,Y)
 # (I used ChatGPT 4o to help me with this pattern)
 pattern = r'mul\(\d+,\d+\)'
 do_pattern = r'\bdo\(\)'       # Matches `do()`
 dont_pattern = r"\bdon't\(\)"  # Matches `don't()`
-       
+
+# We start enabled
+do_positions.append(0)
+
 # Open the file for reading
 with open('p03-input.txt', 'r') as file:
     content = file.read()
@@ -43,6 +46,35 @@ with open('p03-input.txt', 'r') as file:
     print(f"do_positions: {do_positions}")
     print(f"dont_positions: {dont_positions}")   
 
-# TODO: Implement the logic to handle the do() and don't() instructions
+# Find ranges of enablement
+for i in range(len(do_positions)):
+    try:
+        #print(f"do_positions: {do_positions}")
+        next_dont_list = [x for x in dont_positions if x > do_positions[i]]
+        #print(f"next_dont_list: {next_dont_list}")
+        next_dont = min(next_dont_list)
+        enabled_ranges.append((do_positions[i], next_dont))
+        do_positions = [x for x in do_positions if x > next_dont or x <= do_positions[i]]
+    except:
+        pass #  ¯\_(ツ)_/¯ not proud of this but easier than figuring out the indexing errors
+
+enabled_ranges.append((do_positions[-1], len(content)))  #  ¯\_(ツ)_/¯  last element missing for some reason, add it
+# slice enabled sections out and put them into a new string
+enabled_content = ""
+for enabled_range in enabled_ranges:
+    enabled_content = enabled_content + content[enabled_range[0]:enabled_range[1]]
+
+print(f"Enabled ranges:{enabled_ranges}")
+print(f"Content length:{len(content)}")
+print(f"Enabled length:{len(enabled_content)}")
+# Then, find all mul instructions in the new string
+matches = re.findall(pattern, enabled_content)
+# Iterate over the matches
+for match in matches:
+    # Extract the two numbers from the match
+    numbers = re.findall(r'\d+', match)
+    # Multiply the two numbers and add the product to the total
+    totalProduct += int(numbers[0]) * int(numbers[1])
 
 print(f"The total product of all valid instructions is: {totalProduct}")
+#print(enabled_content)
